c7e32f5e6c5ab39b547ee0154ba1c700a539d1b5
YasRam <y.r.a.elnobe@gmail.com>
2025-07-27 16:28:41 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (<>
+   <h1 className="bg-blue-500 text-white p-5 text-center">
+      Tailwind اشتغل أخيرًا 🔥
+    </h1>
+    
+      <h1 className="bg-red-500 text-white p-5 text-center">
+      Tailwind شغال ولا لأ؟ 🔥
+    </h1>
+  </>
+   
+  )
+}
+
+export default App
